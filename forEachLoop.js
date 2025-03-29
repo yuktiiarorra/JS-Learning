3328b934b477ff209726739d0d5614b964172755
Yukti Arora <arorayukti1919@gmail.com>
2025-03-30 01:50:17 +0530

    adds for Each loop

---
+++ forEachLoop.js
@@ -0,0 +1,7 @@
+const subreddits = ["cringe", "books", "chickens", "funny", "pics", "soccer", "gunner"];
+
+const fn = function(subreddit){
+    console.log(subreddit.toUpperCase());
+}
+
+subreddits.forEach(fn);
