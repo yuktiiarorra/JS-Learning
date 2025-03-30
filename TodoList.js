1dc93d2e17360b1e37da4ff2c0468e53067210a8
Yukti Arora <arorayukti1919@gmail.com>
2025-03-31 03:15:49 +0530

    adds todo List

---
+++ TodoList.js
@@ -0,0 +1,35 @@
+let prompt=console.log;
+
+let input = prompt("nonon");
+
+
+const toDo = [];
+
+console.log('HEREEE');
+
+while(input !== "quit" && input !== "q")
+{
+    if(input === "new")
+    {
+        const task = prompt("Enter new todo");
+        console.log(`${task} added to the list!`);
+        toDo.push(task);
+    }
+    else if(input === "list")
+    {
+        console.log("*********");
+        for(let i = 0; i < toDo.length; i++)
+        {
+            console.log(`${i}: ${toDo[i]}`);
+        }
+        console.log("*********");
+    }
+    else if(input === "delete")
+    {
+        const remove = prompt("Enter index of todo to delete");
+        const deleted = toDo.splice(remove, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
+    }
+    input = prompt("What would you like to do?");
+}
+console.log("OK! YOU QUIT THE APP");
